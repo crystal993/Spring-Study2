a897a340c749828a53907b25d1bf1fff7cf1c24f
unknown <ggg7152@gmail.com>
2022-03-14 15:52:02 +0900

    스프링으로 전환하기 AppConfig 스프링 기반으로 변경 MemberApp에 스프링 컨테이너 적용 OrderApp에 스프링 컨테이너 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 스프링컨테이너에서 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
 //        새로운 회원 데이터 생성
         Member member = new Member(1L, "memberA", Grade.VIP);
